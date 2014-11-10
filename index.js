886b4006f3efddd2328422f2b2edc6c3a14153ff
mojoaxel <alex@wunschik.net>
2014-11-10 11:28:46 +0100

    return array of licenses if multible liceses were found

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 var fs = require('fs');
 var path = require('path');
 
-var potentialFilenames = ['LICENSE', 'README', 'README.md', 'README.markdown','license.txt'];
+var potentialFilenames = ['LICENSE', 'license.txt', 'README', 'README.md', 'README.markdown'];
 
-var licenseFromString = function(str){
-    if (str.indexOf('MIT') > -1) {
-        return 'MIT*';
-    } else if (str.indexOf('BSD') > -1) {
-        return 'BSD*';
-    } else if (str.indexOf('Apache License') > -1) {
-        return 'Apache*';    
-    } else if (str.indexOf('Mozilla') > -1) {
-        return 'Mozilla*';
-    } else if (str.indexOf('LGPL') > -1) {
-        return 'LGPL*';
-    } else if (str.indexOf('Affero') > -1) {
-        return 'GPL*';
-    } else if (str.indexOf('GPL') > -1) {
-        return 'GPL*';
-    } else if (str.indexOf('Eclipse') > -1) {
-        return 'Eclipse*';
-    } else if (str.indexOf('Artistic') > -1) {
-        return 'Artistic*';
-    } else if (str.indexOf('DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE') > -1) {
-        return 'WTF*';
-    }  
+var licenseFromString = function(content){
+    var licenses = [];
+    if (content.indexOf('MIT') > -1) licenses.push('MIT*');
+    if (content.indexOf('BSD') > -1) licenses.push('BSD*');
+    if (content.indexOf('Apache License') > -1) licenses.push('Apache*');
+    if (content.indexOf('Mozilla') > -1) licenses.push('Mozilla*');
+    if (content.indexOf('LGPL') > -1) licenses.push('LGPL*');
+    if (content.indexOf('Affero') > -1) licenses.push('GPL*');
+    if (content.indexOf('GPL') > -1) licenses.push('GPL*');
+    if (content.indexOf('Eclipse') > -1) licenses.push('Eclipse*');
+    if (content.indexOf('Artistic') > -1) licenses.push('Artistic*');
+    if (content.indexOf('DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE') > -1) licenses.push('WTF*');
+    return licenses;
 }
 
 module.exports = function(packagePath){
@@ -34,10 +25,14 @@ module.exports = function(packagePath){
             // Do a case-insensitive match to find files named
             // Readme.md or other variations
             if (potentialFilenames[i].toLowerCase() === files[j].toLowerCase()) {
-                var license = licenseFromString(fs.readFileSync(path.resolve(packagePath, files[j]), 'utf8'));
-                if (license) return license;
+                var licenses = licenseFromString(fs.readFileSync(path.resolve(packagePath, files[j]), 'utf8'));
+                
+                // if only one license is found remove array
+                if (licenses && licenses.length === 1) licenses = licenses[0];
+                
+                return licenses;
             }
         }
     }
-    return null;
+    return [];
 }
